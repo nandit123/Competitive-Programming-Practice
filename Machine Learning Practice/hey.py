504d4e620c41a3758a8ac8cb734405f527a99c65
Nandit Mehra <nandit.mehra.15csc@bml.edu.in>
2018-08-18 19:03:51 +0530

    OCR applicartion added

---
+++ Machine Learning Practice/hey.py
@@ -0,0 +1,9 @@
+from sklearn.datasets import load_digits
+digits = load_digits()
+digits.images.shape
+import matplotlib.pyplot as plt
+fig, axes = plt.subplots(10, 10, figsize=(8, 8), subplot_kw={'xticks':[], 'yticks':[]}, gridspec_kw=dict(hspace=0.1, wspace=0.1))
+for i, ax in enumerate(axes.flat):
+    ax.imshow(digits.images[i], cmap='binary', interpolation='nearest')
+    ax.text(0.05, 0.05, str(digits.target[i]),
+        transform=ax.transAxes, color='green')
