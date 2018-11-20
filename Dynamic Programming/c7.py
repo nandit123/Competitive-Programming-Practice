551feeb6d1ee31295f70c4e96fca5d48aa09dd56
Nandit Mehra <nandit.mehra.15csc@bml.edu.in>
2018-11-21 01:26:10 +0530

    Added Dynamic Programming practice codes.

---
+++ Dynamic Programming/c7.py
@@ -0,0 +1,27 @@
+# edit distance by DPP
+def editDistDP(str1, str2, m, n):
+    dp = [[0 for x in range(n+1)] for x in range(m+1)]
+
+    for i in range(m+1):
+        for j in range(n+1):
+
+            if i == 0:
+                dp[i][j] = j
+
+            elif j == 0:
+                dp[i][j] = i
+
+            elif str1[i-1] == str2[j-1]:
+                dp[i][j] = dp[i-1][j-1]
+
+            else: 
+                dp[i][j] =  1+min(dp[i][j-1],   # insert 
+                                  dp[i-1][j],   # remove
+                                  dp[i-1][j-1]) # replace
+    return dp[m][n]
+
+
+str1 = "sunday"
+str2 = "saturday"
+
+print('ediDistance', editDistDP(str1, str2, len(str1), len(str2)))
